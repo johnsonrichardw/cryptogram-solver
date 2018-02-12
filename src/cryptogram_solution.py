551da13b865c46b323bbb9165906a9df16f132a0
Richard Johnson <johnsonrichardw@gmail.com>
2018-02-11 19:14:44 -0800

    Adding docs,tests, and implementation of the crypto solution

---
+++ src/cryptogram_solution.py
@@ -0,0 +1,47 @@
+import string
+import random
+
+class cryptogram_solution():
+    def __init__(self):
+        self.char_list = list(string.ascii_uppercase)
+        self.solution = self.gen_initial_solution()
+
+    def gen_initial_solution(self):
+        solution = {c:c for c in self.char_list}
+        val_list = [solution[k] for k in solution.keys()]
+        random.shuffle(val_list)
+        for k in solution.keys():
+            solution[k] = val_list.pop()
+        return solution
+
+    def get_deciphered_value(self,cipher_char):
+        if cipher_char not in self.solution:
+            return cipher_char
+        else:
+            return self.solution[cipher_char]
+
+    def set_deciphered_value(self,cipher_char,decipher_char):
+        self.solution[cipher_char] = decipher_char
+
+    def swap_values(self,cipher1,cipher2):
+        val = self.get_deciphered_value(cipher1)
+        self.set_deciphered_value(cipher1,self.get_deciphered_value(cipher2))
+        self.set_deciphered_value(cipher2,val)
+
+    def get_random_swap_chars(self):
+        a = random.choice(self.char_list)
+        b = a
+        while a == b:
+            b = random.choice(self.char_list)
+        return a,b
+
+    def mutate(self):
+        a,b = self.get_random_swap_chars()
+        self.swap_values(a,b)
+
+    def decipher(self,ciphertext):
+        cleartext = ''
+        ciphertext = ciphertext.upper()
+        for i in range(len(ciphertext)):
+            cleartext += self.get_deciphered_value(ciphertext[i])
+        return cleartext
---
+++ test/test_crypto_sol.py
@@ -0,0 +1,84 @@
+import pytest
+from cryptogram_solution import cryptogram_solution
+import string
+
+def test_init_class():
+    assert cryptogram_solution() != None
+
+def test_get_map():
+    cs = cryptogram_solution()
+    cmap = cs.gen_initial_solution()
+    keys = sorted(cmap.keys())
+    assert len(keys) == 26
+    expected_vals = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    assert keys == expected_vals
+    for k,v in cmap.items():
+        assert v in expected_vals
+        expected_vals.remove(v)
+    assert len(expected_vals) == 0
+
+def test_map_randomness():
+    expected_vals = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    cs = cryptogram_solution()
+    cmap = cs.gen_initial_solution()
+    val_list = [cmap[k] for k in sorted(cmap.keys())]
+    assert len(val_list) == 26
+    different = 0
+    #Test that the randomized list is actually randomized. So as a simple
+    #heuristic assume the list is randomized if at least 5 elements are
+    #different between the two lists.
+    for i in range(len(expected_vals)):
+        if expected_vals[i] != val_list[i]:
+            different += 1
+    assert different >= 5
+
+def test_swap():
+    cs = cryptogram_solution()
+    a_val = cs.get_deciphered_value('A')
+    b_val = cs.get_deciphered_value('B')
+    assert a_val != b_val
+    cs.swap_values('A','B')
+    assert a_val == cs.get_deciphered_value('B')
+    assert b_val == cs.get_deciphered_value('A')
+
+def test_random_chars():
+    cs = cryptogram_solution()
+    for i in range(100):
+        a, b = cs.get_random_swap_chars()
+        assert a != b
+
+def test_mutate():
+    cs = cryptogram_solution()
+    val_list = [cs.solution[k] for k in sorted(cs.solution.keys())]
+    cs.mutate()
+    new_val_list = [cs.solution[k] for k in sorted(cs.solution.keys())]
+    different = 0
+    for i in range(26):
+        if val_list[i] != new_val_list[i]:
+            different += 1
+    assert different == 2
+
+def test_simple_decipher():
+    expected_vals = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    rot13 = [expected_vals[(i+13)%26] for i in range(len(expected_vals))]
+    cs = cryptogram_solution()
+    for i in range(len(expected_vals)):
+        cs.set_deciphered_value(expected_vals[i],rot13[i])
+    assert cs.decipher('EVPUNEQ') == 'RICHARD'
+
+def test_non_letter_decipher():
+    garbage_string = "A  -G343,.Y!#[]"
+    cleartext = cryptogram_solution().decipher(garbage_string)
+    assert len(garbage_string) == len(cleartext)
+    for i in range(len(garbage_string)):
+        if garbage_string[i] not in string.ascii_uppercase:
+            assert garbage_string[i] == cleartext[i]
+
+def test_case():
+    expected_vals = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    rot13 = [expected_vals[(i+13)%26] for i in range(len(expected_vals))]
+    cs = cryptogram_solution()
+    for i in range(len(expected_vals)):
+        cs.set_deciphered_value(expected_vals[i],rot13[i])
+    assert cs.decipher('evpuneq') == 'RICHARD'
+
